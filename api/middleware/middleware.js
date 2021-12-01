3d98a5ce47258b6135c0e19146f50063fba3981a
Anthony <aamay022@fiu.edu>
2021-12-01 18:11:55 -0500

    first

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,49 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`${req.method} ${req.baseUrl}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+try{
+const user = await User.getById(req.params.id);
+if(user){
+  req.user = user;
+  next();
+} else {
+  next({status: 404, message: "user not found"})
+}
+} catch (error) {
+next(error);
+}
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  if (!req.body.name) {
+    next({ status: 400, message: "missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  if (!req.body.text) {
+    next({ status: 400, message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
+
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,40 +5,103 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require('../middleware/middleware');
+
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
+
+
+
+router.get('/', logger, (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get(req.query)
+  .then(u => {
+    res.status(200).json(u)
+  })
+  .catch(err => {
+    next(err)
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', logger, validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user);
 });
 
-router.post('/', (req, res) => {
+router.post('/', logger, validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  User.insert(req.body)
+  .then(newUser => {
+    res.status(201).json(newUser);
+  })
+  .catch(error => {
+    next(error);
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', logger, validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+
+  User.update(req.params.id, req.body)
+  .then(updatedUser => {
+    res.json(updatedUser)
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', logger, validateUserId, (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  User.remove(req.params.id)
+  .then(deletedUser => {
+    res.json(deletedUser);
+  })
+  .catch(err => {
+    next(err);
+  });
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', logger, validateUserId, (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  User.getUserPosts(req.params.id)
+  .then(p => {
+    res.status(200).json(p);
+  })
+  .catch(err => {
+    next(err);
+  });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validatePost, validateUserId, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  const postInfo = {...req.body, user_id: req.params.id };
+
+  Post.insert(postInfo)
+  .then(newPost => {
+    res.status(210).json(newPost);
+  })
+  .catch(error => {
+    res.status(400).json({
+      error: "missing required text field",
+    })
+  });
 });
 
 // do not forget to export the router
+
+module.exports = router;
+
